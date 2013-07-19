9fbe26afb311378d1fbe10eb110731f6105ec3dd
Marshall <mad.mod.max@gmail.com>
2013-07-19 18:13:00 -0400

    Added basic unit with movement
    Movement currently controlled via arrow keys or WASD

--- src/components.js
+++ src/components.js
@@ -5,6 +5,7 @@ Crafty.c('Grid', {
 			h: Game.map_grid.tile.height
 		})
 	},
+	//
 	at: function(x, y) {
 		if (x === undefined && y === undefined) {
 			return {
@@ -22,9 +23,38 @@ Crafty.c('Grid', {
 	}
 });
 
+Crafty.c('Actor', {
+	init: function() {
+		this.requires('2D, Canvas, Grid');
+	},
+});
+
 Crafty.c('Block', {
 	init: function() {
-		this.requires('2D, Canvas, Grid, Color');
+		this.requires('Actor, Color, Solid');
 		this.color('black');
 	},
+});
+
+Crafty.c('Unit', {
+	init: function() {
+		this.requires('Actor, Fourway, Color, Collision')
+			.fourway(4) // Takes in speed as a param
+			.color('red')
+			.stopOnSolids();
+	},
+	// Stop movement upon collision with a solid entity
+	stopOnSolids: function() {
+		this.onHit('Solid', this.stopMovement);
+		
+		return this;
+	},
+	
+	stopMovement: function() {
+		this._speed = 0;
+		if (this._movement) {
+			this.x -= this._movement.x;
+			this.y -= this._movement.y;
+		}
+	}
 });
--- src/game.js
+++ src/game.js
@@ -18,6 +18,8 @@ Game = {
     start: function() {
         Crafty.init(Game.width(), Game.height());
         Crafty.background('green');
+		
+		Crafty.e('Unit').at(5, 5);
         
         for (var x = 0; x < Game.map_grid.width; x++) {
 			for (var y = 0; y < Game.map_grid.height; y++) {
