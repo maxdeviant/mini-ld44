182d413a728b49c0f1ba6f8e1b34c135c112d5ee
Marshall <mad.mod.max@gmail.com>
2013-07-19 20:09:37 -0400

    Moved main game function to scenes.js

--- src/game.js
+++ src/game.js
@@ -16,21 +16,9 @@ Game = {
     },
     
     start: function() {
-        Crafty.init($(document).width() - 20, $(document).height() - 20);
+        Crafty.init($(document).width() - 50, $(document).height() - 50);
         Crafty.background('green');
 		
-		Crafty.viewport.mouselook(true);
-		
-		Crafty.e('Unit').at(5, 5);
-        
-        for (var x = 0; x < Game.map_grid.width; x++) {
-			for (var y = 0; y < Game.map_grid.height; y++) {
-				var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
-				
-				if (at_edge) {
-					Crafty.e('Block').at(x, y);
-				}
-			}
-		}
+		Crafty.scene('Game');
     }
 }
--- src/scenes.js
+++ src/scenes.js
@@ -1,5 +1,17 @@
 Crafty.scene('Game', function() {
+	Crafty.viewport.mouselook(true);
+		
+	Crafty.e('Unit').at(5, 5);
 	
+	for (var x = 0; x < Game.map_grid.width; x++) {
+		for (var y = 0; y < Game.map_grid.height; y++) {
+			var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
+			
+			if (at_edge) {
+				Crafty.e('Block').at(x, y);
+			}
+		}
+	}
 });
 
 Crafty.scene('Loading', function() {
